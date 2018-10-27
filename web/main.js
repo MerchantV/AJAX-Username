ec1e7d358d985e875a46a681c094f43b5d5356e1
Kyle Geske <stungeye@gmail.com>
2018-10-27 17:02:33 -0500

    Comments in the javascript and php scripts.

--- web/main.js
+++ web/main.js
@@ -1,3 +1,6 @@
+
+// Hides or shows the username available and the user name taken
+// messages using CSS block property.
 function usernameMessage(usernameAvailable) {
   let usernameAvailableMsg = document.querySelector('.username-available-msg');
   let usernameTakenMsg = document.querySelector('.username-taken-msg');
@@ -12,19 +15,27 @@ function usernameMessage(usernameAvailable) {
 }
 
 function checkUsername(event) {
+  // Fetch the username provided by the user in the target input.
   let username = event.target.value;
 
+  // Don't bother checking blank usernames.
   if (username === '') {
     return;
   }
 
+  // AJAX GET request to test the username for availability.
   fetch('username.php?username=' + username)
     .then(function(rawResponse) { 
       return rawResponse.json(); // Promise for parsed JSON.
     })
     .then(function(response) {
+      // If the API check was successful.
       if (response['success']) {
+        // Show the relevant username message (available / taken).
         usernameMessage(response['usernameAvailable'])
+
+        // If the username is take put the focus back on the input
+        // and select all text.
         if (! response['usernameAvailable']) {
           event.target.select();
         }
@@ -32,5 +43,7 @@ function checkUsername(event) {
     });
 };
 
+// Bind the checkUsername function to the onblur event of the username input.
+// When this input loses focus this function will be executed.
 let inputUsername = document.querySelector('#inputUsername');
 inputUsername.onblur = checkUsername;
--- web/username.php
+++ web/username.php
@@ -1,8 +1,29 @@
 <?php
+
+  /* 
+
+  This script pretends to check the availability of a username.
+
+  Normally this would be done by checking the users name in the DB. 
+
+  Returns JSON in this form: 
+
+    { 
+      'success': true/false,
+      'usernameAvailable': true/false
+    }
+
+  Properties:
+  - success             true unless a username is not provided in $_GET['username]
+  - usernameAvailable   true if username is not present in $registered_usernames.
+
+  */ 
+
   $registered_usernames = ['wallyg', 'dglutton', 'stungeye', 'mirage', 'chin', 'alan'];
 
   $response = [
-    'success' => false
+    'success' => false,
+    'usernameAvailable' => false
   ];
 
   if (isset($_GET['username']) && (strlen($_GET['username']) !== 0)) {
@@ -12,6 +33,7 @@
 
   // Set the JSON MIME content type so that it isn't sent as text/html
   header('Content-Type: application/json');
+
   // Encode the $response into JSON and echo.
   echo json_encode($response);
 ?>
