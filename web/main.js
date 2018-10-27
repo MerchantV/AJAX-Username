e87b2f371c719195303b6932386dd20d2f5f2ee2
Kyle Geske <stungeye@gmail.com>
2018-10-27 16:22:23 -0500

    Using the fetch API to test the username.

--- web/main.js
+++ web/main.js
@@ -1,5 +1,20 @@
-let inputUsername = document.querySelector('#inputUsername');
 
-inputUsername.onblur = function() {
-  console.log('blur');
+function checkUsername(event) {
+  let username = event.target.value;
+
+  if (username === '') {
+    return;
+  }
+
+  fetch('username.php?username=' + username)
+    .then(function(rawResponse) { 
+      return rawResponse.json(); // Promise for parsed JSON.
+    })
+    .then(function(response) {
+      console.log(response);
+    });
+
 };
+
+let inputUsername = document.querySelector('#inputUsername');
+inputUsername.onblur = checkUsername;
