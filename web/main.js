ce52e7b57bb1db9e0e3a3a501487d896df14fe34
Kyle Geske <stungeye@gmail.com>
2018-10-27 16:12:04 -0500

    Can I bind an onblur through a property assignment?

--- web/main.js
+++ web/main.js
@@ -1,5 +1,5 @@
 let inputUsername = document.querySelector('#inputUsername');
 
-addEvent(el, 'onblur', function(){ 
-  console.log('Clicked!'); 
-})
+inputUsername.onblur = function() {
+  console.log('blur');
+};
